a8b5b9967f1f38cec7994e2c75b2eb4e578d46a0
Nivetha Suresh <mail.nivethasuresh@gmail.com>
2025-07-22 16:05:41 +0530

    JwtUtill - token generation & validation

---
+++ src/main/java/com/tcs/busbuddy/security/JwtUtil.java
@@ -0,0 +1,53 @@
+package com.tcs.busbuddy.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    private static final String secret = "BusBuddyApp29051999";
+    private final Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+    public String generateToken(String userName) {
+        return Jwts.builder()
+
+                .setSubject(userName)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + (60 * 60 * 1000)))
+                .signWith(key)
+
+                .compact();
+    }
+
+    public Boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+
+                .getBody()
+                .getSubject();
+    }
+
+}
