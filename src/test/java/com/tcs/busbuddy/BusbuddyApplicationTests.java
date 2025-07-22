fa05a175c2f4d195282d0c9aa85dc3eadfe34f4a
Nivetha Suresh <mail.nivethasuresh@gmail.com>
2025-07-22 11:47:37 +0530

    code structure

---
+++ src/test/java/com/tcs/busbuddy/BusbuddyApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tcs.busbuddy;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class BusbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
